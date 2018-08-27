79e184c5bf54e454a153b2a416fe61f94a950970
Matt Townsen <matthewtownsen@gmail.com>
2018-08-27 17:53:47 -0600

    follow group workflow

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,11 +7,13 @@ class EmployeesController < ApplicationController
 
   def new
     @employee = User.new
+    @groups = current_user.company.groups
   end
 
   def create
     @employee = User.new(user_params)
     @employee.company = current_user.company
+    add_groups
 
     respond_to do |format|
       if @employee.save
@@ -28,5 +30,13 @@ class EmployeesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email).merge(password: Devise.friendly_token.first(8))
+    end
+
+    def add_groups
+      params[:user][:groups].each do |group_id|
+        if group_id != ''
+          @employee.groups << Group.find_by_id(group_id)
+        end
+      end
     end
 end
